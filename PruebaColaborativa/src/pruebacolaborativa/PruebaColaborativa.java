0ea9e29a7f77b0ed6fd674afa8bb18e2b2ea35b4
gemilla <gema.garciafdez@gmail.com>
2016-01-17 23:24:45 +0100

    Prueba Git Colaborativa desde gemilla

--- PruebaColaborativa/src/pruebacolaborativa/PruebaColaborativa.java
+++ PruebaColaborativa/src/pruebacolaborativa/PruebaColaborativa.java
@@ -17,6 +17,7 @@ public class PruebaColaborativa {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("usuario geallu");
+        System.out.println("me gustaría colaborar");
     }
     
 }
